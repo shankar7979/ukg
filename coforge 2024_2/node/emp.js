540f6651f3cb26afacecf4be041d124472fd5afd
shankar7979 <shankar7979@gmail.com>
2024-09-17 09:18:11 +0530

    added

--- coforge 2024_2/node/emp.js
+++ coforge 2024_2/node/emp.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 app.use(express.json());
 
+
 app.get("/", (req, resp) => {
     resp.write('hello');
     resp.end();
@@ -28,5 +29,52 @@ app.get("/employee/:eid", (req, resp) => {
     else
         return resp.status(201).send(e)
 })
+// add record post method
+app.post("/employee/", (req, resp) => {
+    console.log('post method')
+    const newemp = {
+        eid: req.body.eid,
+        name: req.body.name,
+        salary: req.body.salary,
+        dob: req.body.dob
+    }
+    if (!newemp.eid) {
+        resp.status(404).send('please add the id')
+    }
+    else if (!newemp.name) {
+        resp.status(404).send('please add the name')
+    }
+    else if (!newemp.salary) {
+        resp.status(404).send('please add the salary')
+    }
+    else if (!newemp.dob) {
+        resp.status(404).send('please add the dob')
+    }
+    emp.push(newemp);
+    resp.send(newemp);
+})
 
+//delete 
+
+app.delete('/employee/:eid', (req, resp) => {
+    if (!req.params.eid || isNaN(req.params.eid)) {
+        return resp.status(404).send('invalid id')
+    }
+    var e = emp.find((a) => {
+        return a.eid === parseInt(req.params.eid)
+    });
+
+    if (!e) {
+        resp.status(404).json({ 'msg': 'employee with ' + req.params.eid + ' id is not present' });
+        //resp.status(404).send('employee with ' + req.params.eid + ' id is not present' );
+    }
+
+    const index = emp.indexOf(e);
+    console.log(e + ' found at ' + index);
+    emp.splice(index, 1);
+
+    //resp.json({ 'msg': 'employee with ' + req.params.eid + 'id is deleted ' });
+    return resp.json(e);
+
+});
 app.listen(3000)    
